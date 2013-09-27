11ffcd83f47dfe9ff66cbaf7454ea11122b62571
Chris Saylor <christopher.saylor@zumba.com>
2013-09-27 09:21:48 -0400

    Client connects to defender namespace.

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ var tracer = require('tracer').colorConsole({
 	level: 'debug'
 });
 
-var socket = io.connect('http://localhost:1337', {
+var defender = io.connect('http://localhost:1337/defender', {
 	query: 'username=player1'
 });
 
-socket.on('handshake', function(data) {
-	tracer.info(data);
-});
-
-socket.on('test_event', function(data) {
-	tracer.debug(data);
-});
-
-socket.on('disconnect', function(data) {
-	tracer.info('Disconnected: Thanks for playing.');
-	process.exit();
-})
+defender
+	.on('handshake', function(data) {
+		tracer.info(data);
+	})
+	.on('test_event', function(data) {
+		tracer.debug(data);
+	})
+	.on('disconnect', function(data) {
+		tracer.info('Disconnected: Thanks for playing.');
+		process.exit();
+	});
