0fe13529ce12fac6436c7f511c1b02564e16ccae
Chris Saylor <cjsaylor@gmail.com>
2013-10-12 11:08:49 -0400

    Started "blabber" lib.
    This abstracts the tracer lib and will handle server output with
    various output options.

--- index.js
+++ index.js
@@ -3,38 +3,37 @@
 // Modules
 var io = require('socket.io-client');
 var _ = require('underscore');
-var tracer = require('tracer').colorConsole({
-	level: 'debug',
-    format : "{{message}}",
-});
 
 // Lib
 var RoundInfo = require('./lib/round_info');
 var Brain = require('./lib/brain');
 var Commander = require('./lib/commander');
+var Blabber = require('./lib/blabber');
 
 // locals
 var defender = io.connect((process.env.HOST || 'http://localhost:8080') + '/defender', {
 	query: 'username=' + require('./strategy').name
 });
-var brain = new Brain(new Commander(defender, tracer));
+var brain = new Brain(new Commander(defender));
+var blab;
 
 defender
 	.on('handshake', function(data) {
-		tracer.info('%s', data.message);
+		Blabber.info(data.message);
 	})
 	.on('round', function(data) {
-		tracer.info('Round %s', data.round);
+		Blabber.info('Round %s', data.round);
+		blab = new Blabber(data);
 		// @todo remove debug output
-		tracer.debug(data);
+		blab.debugOutput();
 
 		brain.onRound(new RoundInfo(data));
 	})
 	.on('death', function(data) {
-		tracer.error('%s', data.message);
-		tracer.warn(data.stats);
+		Blabber.error(data.message);
+		Blabber.warn(data.stats);
 	})
 	.on('disconnect', function() {
-		tracer.info('Disconnected: Thanks for playing.');
+		Blabber.info('Disconnected: Thanks for playing.');
 		process.exit();
 	});
