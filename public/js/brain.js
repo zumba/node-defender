f28903bfb65d94cf47a4387dd89698556ac9d5d8
Chris Saylor <cjsaylor@gmail.com>
2013-10-20 20:43:47 -0400

    Implemented startegy storage in html5 local session storage.

--- public/js/brain.js
+++ public/js/brain.js
@@ -1,12 +1,37 @@
 var Brain = (function() {
-	var strategy;
+	var strategy, getBaseStrategy;
+
+	var _STORAGE_KEY = 'strategy';
+
+	var _baseStrategyID = '#base-strategy';
 
 	function Brain(commander) {
 		this.commander = commander;
 	}
 
+	Brain.getBaseStrategy = function() {
+		return $(_baseStrategyID).html();
+	}
+
+	Brain.retrieveStrategy = function() {
+		var code;
+		if (!window.sessionStorage) {
+			return getBaseStrategy();
+		}
+		code = sessionStorage.getItem(_STORAGE_KEY);
+		return code || getBaseStrategy();
+	}
+
+	Brain.storeStrategy = function(code) {
+		if (!window.sessionStorage) {
+			return;
+		}
+		sessionStorage.setItem(_STORAGE_KEY, code);
+	}
+
 	Brain.prototype.setStrategy = function(code) {
 		// Pad with json
+		Brain.storeStrategy(code);
 		code = 'strategy = function(roundInfo, commander) {' + code + '};';
 		try {
 			eval(code);
