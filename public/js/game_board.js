c50271e65a85aabe2e27139a0c6e6bb7d50de1b0
Juan Basso <jrbasso@gmail.com>
2013-10-26 15:02:15 -0400

    Tons of optimizations
    Reduced the number of math operations
    Created an Enemy object to cache things and handle things more dynamically
    Code is more readable now

--- public/js/game_board.js
+++ public/js/game_board.js
@@ -19,6 +19,7 @@ var GameBoard = (function() {
 		ENEMY_HYPOTENUSE_HALF = ENEMY_HYPOTENUSE / 2,
 		POSITION_OFFSET = Math.sqrt(2 * Math.pow(PROFILE_GRAVATAR_SIZE, 2)) / 2,
 		POSITION_WIDTH = ENEMY_HYPOTENUSE + 2,
+		POSITION_PADDING = (POSITION_WIDTH - ENEMY_ICON_SIZE) / 2,
 		PLAYER_GRAVATAR_DEFAULT = 'http://c.dryicons.com/images/icon_sets/simplistica/png/32x32/user.png',
 		ENEMY_ICONS = {
 			grunt: 'http://www.southeastarrow.com/images/icons/blue-left-arrow.png',
@@ -36,7 +37,10 @@ var GameBoard = (function() {
 		this.spots = [];
 
 		this.radius = POSITION_OFFSET + this.num * POSITION_WIDTH;
+		this.enemyRadius = this.getRadius() + POSITION_PADDING;
 		this.maxOfSpots = Math.floor((2 * Math.PI * (this.radius + POSITION_WIDTH / 2)) / (ENEMY_ICON_SIZE * 1.5));
+		this.spotAngle = 2 * Math.PI / this.maxOfSpots;
+		this.startAngle = Math.asin((ENEMY_ICON_SIZE / 2) / this.radius);
 
 		this.renderMark();
 	};
@@ -48,10 +52,14 @@ var GameBoard = (function() {
 	Position.prototype.getFreeSpot = function() {
 		for (var i = 0; i < this.getMaxSpots(); i++) {
 			if (typeof this.spots[i] === 'undefined' || this.spots[i] === false) {
-				return this.spots[i] = i;
+				return this.spots[i] = {
+					id: i,
+					position: this,
+					enemyAngle: this.spotAngle * i - this.startAngle
+				};
 			}
 		}
-		return -1;
+		return false;
 	};
 
 	Position.prototype.renderMark = function() {
@@ -70,6 +78,14 @@ var GameBoard = (function() {
 		return this.radius;
 	};
 
+	Position.prototype.getEnemyRadius = function() {
+		return this.enemyRadius;
+	};
+
+	Position.prototype.getInitialAngle = function() {
+		return this.startAngle;
+	};
+
 	Position.prototype.setSpot = function(spot, enemy) {
 		if (spot >= this.maxOfSpots) {
 			return;
@@ -81,6 +97,64 @@ var GameBoard = (function() {
 		this.spots = [];
 	};
 
+	function Enemy(mob) {
+		this.mob = mob;
+		this.spot = null;
+		this.image = null;
+	};
+
+	Enemy.prototype.render = function() {
+		var pos = _positions[this.mob.position];
+		var posSpot = pos.getFreeSpot();
+		if (posSpot === false) {
+			// No spot available
+			return;
+		}
+		pos.setSpot(posSpot.id, this);
+		this.spot = posSpot;
+
+		var enemy = this,
+			imageObj = new Image();
+		imageObj.onload = function() {
+			var enemyRadius = pos.getEnemyRadius();
+				posX = enemyRadius * Math.cos(posSpot.enemyAngle),
+				posY = enemyRadius * Math.sin(posSpot.enemyAngle);
+
+			enemy.image = new Kinetic.Image({
+				x: _boardCenter.x + posX,
+				y: _boardCenter.y + posY,
+				image: imageObj,
+				rotation: posSpot.enemyAngle,
+				width: ENEMY_ICON_SIZE,
+				height: ENEMY_ICON_SIZE
+			});
+			_boardLayer.add(enemy.image);
+			_boardStage.add(_boardLayer);
+		};
+		imageObj.src = ENEMY_ICONS[this.mob.type];
+	};
+
+	Enemy.prototype.getRotation = function() {
+		return this.image === null ? 0 : this.image.getRotation();
+	};
+
+	Enemy.prototype.getCenterPoint = function() {
+		if (this.image === null) {
+			return { x: 0, y: 0};
+		}
+
+		var a45 = Math.PI / 4; // 45 deg to rad
+		return {
+			x: this.image.getX() + (Math.cos(a45 + this.image.getRotation() - this.spot.position.getInitialAngle()) * ENEMY_HYPOTENUSE_HALF),
+			y: this.image.getY() + (Math.sin(a45 + this.image.getRotation() - this.spot.position.getInitialAngle()) * ENEMY_HYPOTENUSE_HALF),
+		};
+	};
+
+	Enemy.prototype.kill = function() {
+		this.spot.position.setSpot(this.spot.id, false);
+		this.image.remove();
+	};
+
 	var renderMobs = function(mobs) {
 		_.each(mobs, function(mob) {
 			if (typeof _enemies[mob.id] !== 'undefined') {
@@ -88,40 +162,8 @@ var GameBoard = (function() {
 				return;
 			}
 
-			var pos = _positions[mob.position];
-			var posSpot = pos.getFreeSpot();
-			if (posSpot < 0) {
-				// No spot available
-				return;
-			}
-
-			// Calculate the position on board
-			var spotAngle = 360 / pos.getMaxSpots();
-			var spotRad = spotAngle * MATH_DEG_TO_RAD;
-			var marginSpace = (POSITION_WIDTH - ENEMY_ICON_SIZE) / 2;
-			var radius = pos.getRadius() + marginSpace;
-			var startAngle = Math.asin((ENEMY_ICON_SIZE / 2) / radius);
-			var enemyRad = spotRad * posSpot - startAngle;
-
-			var posX = radius * Math.cos(enemyRad);
-			var posY = radius * Math.sin(enemyRad);
-
-			var imageObj = new Image();
-			imageObj.onload = function() {
-				_enemies[mob.id] = new Kinetic.Image({
-					x: _boardCenter.x + posX,
-					y: _boardCenter.y + posY,
-					image: imageObj,
-					rotation: enemyRad,
-					width: ENEMY_ICON_SIZE,
-					height: ENEMY_ICON_SIZE
-				});
-				pos.setSpot(posSpot, _enemies[mob.id]);
-				_enemies[mob.id].boardPosition = posSpot;
-				_enemies[mob.id].startAngle = startAngle;
-				_boardLayer.add(_enemies[mob.id]);
-			};
-			imageObj.src = ENEMY_ICONS[mob.type];
+			_enemies[mob.id] = new Enemy(mob);
+			_enemies[mob.id].render();
 		});
 	};
 
@@ -146,10 +188,7 @@ var GameBoard = (function() {
 			});
 			_boardLayer.add(attackLine);
 
-			var enemyCenter = {
-				x: enemy.getX() + (Math.cos(45 * MATH_DEG_TO_RAD + enemy.getRotation() - enemy.startAngle) * ENEMY_HYPOTENUSE_HALF),
-				y: enemy.getY() + (Math.sin(45 * MATH_DEG_TO_RAD + enemy.getRotation() - enemy.startAngle) * ENEMY_HYPOTENUSE_HALF),
-			};
+			var enemyCenter = enemy.getCenterPoint();
 			var diffX = enemyCenter.x - attackLine.getX(),
 				diffY = enemyCenter.y - attackLine.getY();
 			var anim = new Kinetic.Animation(function(frame) {
@@ -158,8 +197,7 @@ var GameBoard = (function() {
 					attackLine.remove();
 
 					if (isEnemyDead) {
-						_positions[attack.position].setSpot(enemy.boardPosition, false);
-						enemy.remove();
+						enemy.kill();
 						delete _enemies[enemyId];
 					}
 					return;
