10d9c883c6f61de3e89017b8403bf457d3268b68
Chris Saylor <christopher.saylor@zumba.com>
2013-10-28 22:24:13 -0400

    Organize the server and add static template pages.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var mongoStore = require('connect-mongo')(express);
 var TwitterOauth = require('./lib/twitter_oauth.js');
 
 // Local vars
-var sessionConfig;
+var sessionConfig, router;
 
 // Configuration
 var config = {
@@ -46,7 +46,15 @@ app.set('view options', {layout: true});
 app.set('view engine', 'jade');
 app.locals.gaAccount = config.gaAccount;
 app.use(app.router);
-// 404
+
+// Middleware
+app.use(function(req, res, next) {
+	if (config.secure && req.headers['x-forwarded-proto'] != 'https') {
+		res.redirect(config.secureUrl + req.url);
+		return;
+	}
+	next();
+});
 app.use(function(req, res) {
 	res.status(404);
 	res.render('404', {
@@ -54,94 +62,98 @@ app.use(function(req, res) {
 	});
 });
 
-// Express Routes
-app.all('*', function(req, res, next) {
-	if (config.secure && req.headers['x-forwarded-proto'] != 'https') {
-		res.redirect(config.secureUrl + req.url);
-		return;
-	}
-	next();
-});
-
-app.get('/', function(req, res) {
-	if (typeof req.session.username !== 'undefined') {
-		res.redirect('/game');
-		return;
-	}
-	res.render('index');
-});
-
-app.post('/anonymous', function(req, res) {
-	req.session.username = req.body.username
-	res.redirect('/game');
-});
-
-app.get('/oauth/connect', function(req, res) {
-	if (!process.env.TWITTER_CONSUMER_KEY || !process.env.TWITTER_CONSUMER_SECRET) {
-		console.error('Twitter consumer key or secret not defined.');
-		res.redirect('/');
-		return;
-	}
-	TwitterOauth.consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-		if (error) {
-			res.send("Error getting OAuth request token : " + error, 500);
+// Routing methods
+router = {
+	root: function(req, res) {
+		if (typeof req.session.username !== 'undefined') {
+			res.redirect('/game');
 			return;
-		} else {
-			req.session.oauthRequestToken = oauthToken;
-			req.session.oauthRequestTokenSecret = oauthTokenSecret;
-			res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + req.session.oauthRequestToken);      
 		}
-	});
-});
-
-app.get('/oauth/callback', function(req, res) {
-	TwitterOauth.consumer().getOAuthAccessToken(
-		req.session.oauthRequestToken, 
-		req.session.oauthRequestTokenSecret, 
-		req.query.oauth_verifier,
-		function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+		res.render('index');
+	},
+	anonymous: function(req, res) {
+		req.session.username = req.body.username
+		res.redirect('/game');
+	},
+	oauthConnect: function(req, res) {
+		if (!process.env.TWITTER_CONSUMER_KEY || !process.env.TWITTER_CONSUMER_SECRET) {
+			console.error('Twitter consumer key or secret not defined.');
+			res.redirect('/');
+			return;
+		}
+		TwitterOauth.consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
 			if (error) {
-				console.error(error, results);
-				res.send('Error getting OAuth access token', 500);
+				res.send("Error getting OAuth request token : " + error, 500);
 				return;
+			} else {
+				req.session.oauthRequestToken = oauthToken;
+				req.session.oauthRequestTokenSecret = oauthTokenSecret;
+				res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + req.session.oauthRequestToken);      
 			}
-			req.session.oauthAccessToken = oauthAccessToken;
-			req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-
-			TwitterOauth.consumer().get(
-				"https://api.twitter.com/1.1/account/verify_credentials.json", 
-				req.session.oauthAccessToken, 
-				req.session.oauthAccessTokenSecret, 
-				function (error, data, response) {
-					if (error) {
-						console.error(error);
-						res.send('Error getting twitter information.', 500);
-						return;
-					}
-					req.session.twitter = _.pick(JSON.parse(data), ['screen_name', 'profile_image_url_https', 'lang'])
-					req.session.username = req.session.twitter.screen_name;
-					res.redirect('/game');
+		});
+	},
+	oauthCallback: function(req, res) {
+		TwitterOauth.consumer().getOAuthAccessToken(
+			req.session.oauthRequestToken, 
+			req.session.oauthRequestTokenSecret, 
+			req.query.oauth_verifier,
+			function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+				if (error) {
+					console.error(error, results);
+					res.send('Error getting OAuth access token', 500);
+					return;
 				}
-			);
-		}
-	);
-});
+				req.session.oauthAccessToken = oauthAccessToken;
+				req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
 
-app.get('/logout', function(req, res) {
-	req.session.destroy();
-	res.redirect('/');
-});
-
-app.get('/game', function(req, res) {
-	if (typeof req.session.username === 'undefined') {
+				TwitterOauth.consumer().get(
+					"https://api.twitter.com/1.1/account/verify_credentials.json", 
+					req.session.oauthAccessToken, 
+					req.session.oauthAccessTokenSecret, 
+					function (error, data, response) {
+						if (error) {
+							console.error(error);
+							res.send('Error getting twitter information.', 500);
+							return;
+						}
+						req.session.twitter = _.pick(JSON.parse(data), ['screen_name', 'profile_image_url_https', 'lang'])
+						req.session.username = req.session.twitter.screen_name;
+						res.redirect('/game');
+					}
+				);
+			}
+		);
+	},
+	logout: function(req, res) {
+		req.session.destroy();
 		res.redirect('/');
+	},
+	game: function(req, res) {
+		if (typeof req.session.username === 'undefined') {
+			res.redirect('/');
+		}
+		res.render('game', {
+			host: config.host,
+			username: req.session.username,
+			secure: config.secure,
+			token: req.session.oauthAccessToken,
+			secret: req.session.oauthAccessTokenSecret
+		});
 	}
-	res.render('game', {
-		host: config.host,
-		username: req.session.username,
-		secure: config.secure,
-		token: req.session.oauthAccessToken,
-		secret: req.session.oauthAccessTokenSecret
+};
+
+// Routes
+app.get('/', router.root);
+app.post('/anonymous', router.anonymous);
+app.get('/oauth/connect', router.oauthConnect);
+app.get('/oauth/callback', router.oauthCallback);
+app.get('/logout', router.logout);
+app.get('/game', router.game);
+
+// Route static static pages
+_.each(['how-to-play'], function(template) {
+	app.get('/' + template, function(req, res) {
+		res.render(template);
 	});
 });
 
