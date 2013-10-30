db84c552a34cafa31107e6d5ad745254a96ec95d
Stephen Young <young.steveo@gmail.com>
2013-10-29 23:17:53 -0400

    Moved libs to lib folder and started loading dynamically

--- public/js/config.js
+++ public/js/config.js
@@ -2,9 +2,24 @@
 requirejs.config({
 	baseUrl : '/js',
 	paths : {
-		jquery : '//http://code.jquery.com/jquery-1.10.2.min'
+		// lib
+		blabber : 'lib/blabber',
+		brain : 'lib/brain',
+		client : 'lib/client',
+		commander : 'lib/commander',
+		gameBoard : 'lib/game_board',
+		roundInfo : 'lib/round_info',
+
+		// vendor
+		bootstrap : '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min',
+		jquery : '//code.jquery.com/jquery-1.10.2.min',
+		underscore : '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min'
 	},
-	shim : {}
+	shim : {
+		bootstrap : ['jquery'],
+		main : ['bootstrap'],
+		underscore : { exports : '_' }
+	}
 });
 
-require('main');
+require(['main']);
---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+require(
+	['jquery'],
+	function($){
+		$(function() {
+			$('.main').on('submit', '.register-form', function(e) {
+				var input = $(e.currentTarget).find('input');
+				if (!/^[\w\-\(\) ]+$/.test(input.val())) {
+					e.preventDefault();
+					e.stopPropagation();
+					input
+						.tooltip({
+							'title': 'Invalid username.',
+							'trigger': 'manual'
+						})
+						.tooltip('show');
+				}
+			});
+		});
+	}
+);
