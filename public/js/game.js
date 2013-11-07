20e76b89a3854ec6f1fe0d88d354125dc8ff87d5
Stephen Young <young.steveo@gmail.com>
2013-11-06 20:25:30 -0500

    Cleaned up game.js; Event delegation, DOM caching, etc.

--- public/js/game.js
+++ public/js/game.js
@@ -5,56 +5,63 @@ define(
 		return function(){
 			var editor = ace.edit("editor");
 			var startedGames = 0;
+			var editorParent = $('#editor-panel').parent();
+			var gameBoardParent = $('#game-board-panel').parent();
+
 			editor.setTheme("ace/theme/monokai");
 			editor.getSession().setMode("ace/mode/javascript");
 			editor.getSession().setValue(Brain.retrieveStrategy());
-			$('#toggle-game-board').on('click', function(e) {
-				e.preventDefault();
-				$('#editor-panel').parent().addClass('hidden');
-				$('#game-board-panel').parent().removeClass('hidden');
-			});
-			$('#toggle-editor').on('click', function(e) {
-				e.preventDefault();
-				$('#game-board-panel').parent().addClass('hidden');
-				$('#editor-panel').parent().removeClass('hidden');
-			});
-			$('#toggle-sound').on('click', function(e) {
-				var $this = $(this);
 
-				e.preventDefault();
-				if ($this.data('status') === 'muted') {
-					GameBoard.mute = false;
-					$this.data('status', 'unmuted');
-					$this.html('<i class="icon-volume-up" style="font-size: 1.4em"/>');
-				} else {
-					GameBoard.mute = true;
-					$this.data('status', 'muted');
-					$this.html('<i class="icon-volume-off" style="font-size: 1.4em"/>');
-				}
-			});
-			$('#start').on('click', function(e) {
-				if (typeof ga === 'function') {
-					ga('send', 'event', 'game', 'start', 'Start Game', ++startedGames);
-				}
-				$('#editor-panel').parent().addClass('hidden');
-				$('#game-board-panel').parent().removeClass('hidden');
-				e.preventDefault();
-				Client.start(
-					window.username,
-					editor.getSession().getValue(),
-					{
-						token: window.token,
-						secret: window.secret
+			$(document)
+				.on('click', '#toggle-game-board', function(e){
+					e.preventDefault();
+					editorParent.addClass('hidden');
+					gameBoardParent.removeClass('hidden');
+				})
+				.on('click', '#toggle-editor', function(e){
+					e.preventDefault();
+					gameBoardParent.addClass('hidden');
+					editorParent.removeClass('hidden');
+				})
+				.on('click', '#toggle-sound', function(e){
+					var button = $(e.currentTarget);
+					var muted = button.data('status') === 'muted';
+
+					e.preventDefault();
+					GameBoard.mute = !muted; // toggle mute
+					button
+						.children('i')
+						.removeClass()
+						.addClass(muted ? 'icon-volume-up' : 'icon-volume-off')
+						.end()
+						.data('status', muted ? 'unmuted' : 'muted');
+				})
+				.on('click', '#start', function(e) {
+					e.preventDefault();
+
+					if (typeof ga === 'function') {
+						ga('send', 'event', 'game', 'start', 'Start Game', ++startedGames);
 					}
-				);
-			});
-			$('#reset').on('click', function(e) {
-				e.preventDefault();
-				if (!confirm('Are you sure you want to reset your strategy?')) {
-					return;
-				}
-				editor.getSession().setValue(Brain.getBaseStrategy());
-			});
+
+					editorParent.addClass('hidden');
+					gameBoardParent.removeClass('hidden');
+					Client.start(
+						window.username,
+						editor.getSession().getValue(),
+						{
+							token: window.token,
+							secret: window.secret
+						}
+					);
+				})
+				.on('click', '#reset', function(e) {
+					e.preventDefault();
+					if (!confirm('Are you sure you want to reset your strategy?')) {
+						return;
+					}
+					editor.getSession().setValue(Brain.getBaseStrategy());
+				});
+
 			GameBoard.boardSetup();
 			// Assign a global error handler from the client
 			window.onerror = Client.onerror;
